8c54c372a363886c9547e258bf239977451cc592
IMuhammadDev <im2745545@gmail.com>
2023-10-08 12:19:54 +0500

    gunicorn

--- playground/views.py
+++ playground/views.py
@@ -4,11 +4,19 @@ from django.views.decorators.cache import cache_page
 from django.shortcuts import render
 from rest_framework.views import APIView
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HelloView(APIView):
     @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = request.get("https://httpbin.org/delay/2")
-        data = response.json()
+        try:
+            logger.info("calling httpbin")
+            response = requests.get("https://httpbin.org/delay/2")
+            logger.info("recived the responce")
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical("httpbin is ofline")
         return render(request, "hello.html", {"name": "Mosh"})
--- storefront/celery.py
+++ storefront/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings.dev")
 
 celery = Celery("sotrefront")
 celery.config_from_object("django.conf:settings", namespace="CELERY")
